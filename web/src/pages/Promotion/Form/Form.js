8846ddfa389b97d1bbcd3651292d6f98988b9133
carlosrafp <carlosrafp@gmail.com>
2021-03-13 05:38:17 -0800

    create form done

--- web/src/pages/Promotion/Form/Form.js
+++ web/src/pages/Promotion/Form/Form.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
+import PromotionForm from 'components/Promotion/Form/Form';
+import UIContainer from 'components/IU/Container/Container'
 
 const PagesPromotionForm = () => {
     const { id } = useParams();
+
     return (
-        <div>
-            FORM
-            {id && <div>id: {id}</div>}
-        </div>
+        <UIContainer>
+            <PromotionForm />
+        </UIContainer>
     );
 }
 
